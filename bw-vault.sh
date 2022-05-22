722f02c28dafa15f65174c7a6f8474c12bf8a0ab
MateoPeri <mateoperi@pm.me>
2022-05-22 22:45:51 +0200

    added backup playbook

--- bw-vault.sh
+++ bw-vault.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+# there is a bug in bitwarden
+# bw always reports the vault as locked
+# although the session is set
+# see https://github.com/bitwarden/cli/issues/480
+[[ "$(bw unlock --check --quiet)" = *" locked"* ]] && export BW_SESSION="$(bw unlock --raw)"
 bw get password vault-infra
